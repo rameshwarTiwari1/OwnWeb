68a30945c53aecded072d0ed612478fe361779f2
VIKAS TIWARI <rameshwar.kes@gmail.com>
2021-12-15 20:34:00 +0530

    first commit

---
+++ index1.php
@@ -0,0 +1,39 @@
+<?php
+
+$conn = mysqli_connect("localhost", "root", "", "staff");
+
+ // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+          
+
+          // Taking all 5 values from the form data(input)
+        $name =  $_REQUEST['name'];
+        $email = $_REQUEST['email'];
+        $subject = $_REQUEST['subject'];
+        $Number = $_REQUEST['message'];
+
+
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO server  VALUES ('$name', 
+            '$email','$subject','$message')";
+          
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>data stored in a database successfully." 
+                . " Please browse your localhost php my admin" 
+                . " to view the updated data</h3>"; 
+  
+            echo nl2br("\n$name\n $email\n "
+                . "$subject\n $message\n");
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+          
+        // Close connection
+        mysqli_close($conn);
+        ?>
+       
